421e8efb792a357f180af93d2fb4f0c90709fb3b
Akito Mochizuki <ak-mochi@iij.ad.jp>
2013-03-14 15:32:10 +0900

    remove unused method

--- lib/msh/api/abstract_api.rb
+++ lib/msh/api/abstract_api.rb
@@ -14,33 +14,22 @@ module Msh
 
         @api_request = request
         set_param
-        validate
+#        validate
 
       end
 
-      def validate
-        self.private_methods.map{|sym| sym.to_s}.grep(/\Avalidate_.+\z/).each do |validate_method|
-          eval validate_method
-        end
-
-        if @errors
-          @errors.each do |error|
-            puts error
-          end
-        end
-
-      end
-
-      def parse_single_arg(attr)
-        if idx = @api_request.index(attr)
-          @api_request.delete_at(idx)
-          @api_request.delete_at(idx)
-        end
-      end
-
-      def parse_multiple_args(attr)
-
-      end
+#      def validate
+#        self.private_methods.map{|sym| sym.to_s}.grep(/\Avalidate_.+\z/).each do |validate_method|
+#          eval validate_method
+#        end
+#
+#        if @errors
+#          @errors.each do |error|
+#            puts error
+#          end
+#        end
+#
+#      end
 
     end
   end
