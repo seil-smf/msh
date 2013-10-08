fa224a8dc7867cff61e70cd5ed4eec8d453a1436
Akito Mochizuki <ak-mochi@iij.ad.jp>
2013-10-08 18:53:15 +0900

    Request系APIを実行する際のポーリング回数をmshrcで指定できるように変更  - キーはpoll_count  - 例) poll_count: 10  - デフォルト値は5

--- lib/msh/constants.rb
+++ lib/msh/constants.rb
@@ -1,6 +1,6 @@
 module Msh
   module Constants
-    REQUEST_MAX_POLL = 5
+    REQUEST_MAX_POLL = ($conf[:poll_count].to_i if $conf[:poll_count]) || 5
     REQUEST_POLL_INTERVAL = ($conf[:request_interval].to_i if $conf[:request_interval]) || 3
     REQUEST_TARGET_TIME = nil
 
