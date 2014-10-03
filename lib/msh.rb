a83069f64620bddf2aeb4b7795add70ff6a30219
Akito Mochizuki <ak-mochi@iij.ad.jp>
2014-10-03 11:10:20 +0900

    msh起動時に設定ファイル（mshrc）を指定するオプションを追加

--- lib/msh.rb
+++ lib/msh.rb
@@ -1,26 +1,44 @@
-
 require 'pp'
 require 'awesome_print'
 require 'colorize'
+
 require 'msh/msh'
 
 module Msh
-
   def self.start(argv)
-    if RUBY_VERSION >= '1.9'
-      shell = Msh::Cli.new
-      if argv.empty?
-        shell.run
-      else
-        if idx = argv.index("user_code")
-          argv.delete_at(idx)
-          $conf[:user_code] = argv.delete_at(idx)
-        end
-        shell.non_interactive_run(argv)
-      end
-    else
+    if RUBY_VERSION < '1.9'
       $stderr.puts "Ruby version 1.9.* is required to run msh."
+      exit 1
     end
 
+    shell = Msh::Cli.new
+
+    parse_args(argv)
+
+    if argv.empty?
+      # invoke msh as interactive mode
+      shell.run
+    else
+      # invoke msh as non-interactive mode
+      shell.non_interactive_run(argv)
+    end
   end
+
+  def self.parse_args(argv)
+    if idx = argv.index("--mshrc")
+      argv.delete_at(idx)
+      $conf.set_mshrc(argv.delete_at(idx))
+      begin
+        $conf.read
+      rescue => e
+        p e
+      end
+    end
+
+    if idx = argv.index("user_code")
+      argv.delete_at(idx)
+      $conf[:user_code] = argv.delete_at(idx)
+    end
+  end
+
 end
